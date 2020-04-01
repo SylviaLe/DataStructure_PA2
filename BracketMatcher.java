af5de047329ab4649bdd3fe6c8c46c38c8d94759
Khe Le <56932982+KheVLe@users.noreply.github.com>
2020-04-01 18:06:32 +0700

    Update BracketMatcher.java

--- BracketMatcher.java
+++ BracketMatcher.java
@@ -1,24 +1,27 @@
 public class BracketMatcher
 {
-    private static String opening = "([{<";  //the requirement here actually ask for an array of char
-    private static String ending = ")]}>";   //but if then, line 9's indexOf method will generate an error
+    
+    private static char[] opening = {'(','[','{','<'};  // create an array of opening brackets
+    private static char[] ending = {')',']','}','>'};  // create an array of closing brackets
     private static SinglyLinkedStack<Character> stack = new SinglyLinkedStack<>();
+    
     private static boolean isOpeningBracket(char c)  //since not used elsewhere, declared as private
     {
-        if (opening.indexOf(c) != -1) //if the char c is presented in the hardcoded brackets
+        // convert char array to String using method valueOf(char[] data)
+        if (String.valueOf(opening).indexOf(c) != -1) //if the char c is presented in the hardcoded brackets
             return true;
         return false;
     }
 
     private static boolean isClosingBracket(char c) //similar explanation above
     {
-        if (ending.indexOf(c) != -1)
+        if (String.valueOf(ending).indexOf(c) != -1)
             return true;
         return false;
     }
     private static boolean corresponds(char open, char close)
     {
-        if (opening.indexOf(open) == ending.indexOf(close))  //if the index of the bracket in the hardcode is similar
+        if (String.valueOf(opening).indexOf(open) == String.valueOf(ending).indexOf(close))  //if the index of the bracket in the hardcode is similar
             return true;                        //then it's a match
         return false;
     }
