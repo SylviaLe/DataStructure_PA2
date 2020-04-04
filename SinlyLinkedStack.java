ed4ab96a8f323dda3c63c84d092e932f5b36d0be
Khe Le <56932982+KheVLe@users.noreply.github.com>
2020-04-04 18:45:56 +0700

    Update SinlyLinkedStack.java

--- SinlyLinkedStack.java
+++ SinlyLinkedStack.java
@@ -6,20 +6,23 @@ import java.util.Scanner;
 public class BracketMatcher
 {
     //The array of characters (of type char) holding the opening brackets    
-    private static char[] opening = {'(','[','{','<'};  
+    private char[] opening = {'(','[','{','<'};  
 
     //The array of characters holding the corresponding closing brackets 
-    private static char[] ending = {')',']','}','>'}; 
+    private char[] ending = {')',']','}','>'}; 
 
     //New empty stack implemented using singly linked list
-    private static SinglyLinkedStack<Character> stack = new SinglyLinkedStack<>();
+    private SinglyLinkedStack<Character> stack = new SinglyLinkedStack<>();
+
+    //New empty stack implemented using singly linked list to store corresponding indices of elements in stack
+    private SinglyLinkedStack<Integer> indices = new SinglyLinkedStack<>();
     
     /**
      * Return whether a character is an opening bracket
      * @param c a character from the input string
      * @return true iff the character c is an opening bracket, false otherwise
     */ 
-    private static boolean isOpeningBracket(char c)  //since not used elsewhere, declared as private
+    private boolean isOpeningBracket(char c)  //since not used elsewhere, declared as private
     {
         // convert char array to String using method valueOf(char[] data)
         if (String.valueOf(opening).indexOf(c) != -1) //if the char c is presented in the hardcoded brackets
@@ -32,7 +35,7 @@ public class BracketMatcher
      * @param c a character from the input string
      * @return true iff the character c is a closing bracket, false otherwise
     */ 
-    private static boolean isClosingBracket(char c) //similar explanation above
+    private boolean isClosingBracket(char c) //similar explanation above
     {
         if (String.valueOf(ending).indexOf(c) != -1)
             return true;
@@ -45,7 +48,7 @@ public class BracketMatcher
      * @param close a closing bracket character
      * @return true iff the character open is a bracket that corresponds to the closing bracket character close
     */ 
-    private static boolean corresponds(char open, char close)
+    private boolean corresponds(char open, char close)
     {
         if (String.valueOf(opening).indexOf(open) == String.valueOf(ending).indexOf(close))  //if the index of the bracket in the hardcode is similar
             return true;                        //then it's a match
@@ -57,33 +60,42 @@ public class BracketMatcher
      * @param s a String object
      * @return true iff the string s has brackets that are all matched up properly
     */
-    public static boolean checkBrackets(String s)
+    public boolean checkBrackets(String s)
     {
-        int current = 0;
         for(int i = 0; i < s.length(); i++)  //loop through the entire string
         {
             if(isOpeningBracket(s.charAt(i)))
             {
                 stack.push(s.charAt(i));  //if an opening bracket is encountered, push
-                current = i;
+                indices.push(i);
             }
             else if((isClosingBracket(s.charAt(i))))
             {
-                current = i;
                 if(stack.isEmpty()) //the only case that this will be executed is when the number of closing brackets > number opening brackets.
                 {                   
                     System.out.println("Error at position " + i + ':' + s.charAt(i)); //so the prob is at the final bracket
                     return false;
                 }
-                else if(!corresponds(s.charAt(i), stack.pop()))  //pop() is called here, that's why the list can be empty at the end
+                else if(!corresponds(stack.pop(), s.charAt(i)) || indices.pop() == -1)  //pop() is called here, that's why the list can be empty at the end
                 {
                     System.out.println("Error at position " + i + ':' + s.charAt(i));  //error will be at the place where return false
                     return false;
                 }
             }
         }
-        if(stack.size() > 0) System.out.println("Error at position " + (current - stack.size() + 1) + ':' + s.charAt(current - stack.size() + 1)); // if opening brackets > closing brackets
-        return stack.isEmpty();  //add some line to indicate if it's a match
+
+        if(stack.size() > 0) 
+        {
+            while(stack.size() != 1) 
+            {
+                stack.pop();
+                indices.pop();
+            }
+            System.out.println("Error at position " + indices.pop() + ':' + stack.pop()); // if opening brackets > closing brackets
+            return false; 
+        }
+        else
+            return true; // if all brackets are matched up correctly
     }
 }
 
